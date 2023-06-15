26b5175f261e6137cdbcffc75f1b690d2cd05b4f
AdelinaMakarevich <110402319+AdelinaMakarevich@users.noreply.github.com>
2023-06-15 22:24:09 +0300

    fixes in error handling
    необходимо называть переменные таким образом чтобы было понятно и через 10 лет, такие переменные как res, div,form, они не определяют для чего служит эта переменная (исправлено)
    
    необходимо написать функцию debounce, и ее использовать (регулирование запросов было, вывела в отдельную функцию)
    
    там не должно быть тэга <a>, это все кнопки (исправлено)
    
    нужно показать что пользователь, ввел не существующее поисковое значение, и нужно показать текстом что нет такого, прям под поисковым полем (вывожу ошибку не через alert, а текстом внизу)

--- index.js
+++ index.js
@@ -1,61 +1,93 @@
-let form = document.getElementById("formData");
+let formValue = document.getElementById("formData");
 let cooldown = true;
 function runScript(e) {
   if (e.keyCode == 13) {
-    fetchData();
+    debounce();
     return false;
   }
 }
-function fetchData() {
-  let url = `https://api.github.com/search/repositories?q=${form.value}&per_page=5`;
-  if (form.value != 0 && form.value != /^\s+$/ && cooldown == true) {
+function debounce() {
+  let url = `https://api.github.com/search/repositories?q=${formValue.value}&per_page=5`;
+  if (cooldown == true) {
     cooldown = false;
     setTimeout(() => (cooldown = true), 500);
-    fetch(url, {
-      method: "GET",
-      headers: {
-        Accept: "application/vnd.github+json",
-        "X-GitHub-Api-Version": "2022-11-28",
-        "Content-Type": "text/plain;charset=UTF-8",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        let result = data.items;
-        let div = document.getElementById("res");
-        div.innerHTML = "";
-        let secondDiv = document.getElementById("save");
-        div.insertAdjacentHTML("beforeend", `<a>${result[0].name}</a>`);
-        div.insertAdjacentHTML("beforeend", `<a>${result[1].name}</a>`);
-        div.insertAdjacentHTML("beforeend", `<a>${result[2].name}</a>`);
-        div.insertAdjacentHTML("beforeend", `<a>${result[3].name}</a>`);
-        div.insertAdjacentHTML("beforeend", `<a>${result[4].name}</a>`);
-        div.onclick = function (event) {
-          let target = event.target;
-          if (target.tagName == "A") {
-            let res = result.filter((data) => data.name == target.innerHTML);
-            res = res[0];
-            secondDiv.insertAdjacentHTML(
-              "beforeend",
-              `<section><div class='saved_block'>
-              Name: ${res.name}<br>Owner: ${res.owner.login}<br>
-              Stars: ${res.stargazers_count}</div><button class='cross'></button></section>`
-            );
-            div.innerHTML = "";
-            form.value = "";
-          }
-        };
-        secondDiv.onclick = function (event) {
-          let target = event.target;
-          if (target.tagName == "BUTTON") {
-            console.log(event.parent);
-            target.parentNode.remove();
-          }
-        };
-      })
-      .catch((err) => alert(err.name));
-  } else {
-    let div = document.getElementById("res");
-    div.innerHTML = "";
+    if (formValue.value != 0 && formValue.value != /^\s+$/) {
+      fetchData(url);
+    } else {
+      let outcome = document.getElementById("searchResult");
+      outcome.innerHTML = "";
+    }
   }
+}
+function fetchData(url) {
+  fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/vnd.github+json",
+      "X-GitHub-Api-Version": "2022-11-28",
+      "Content-Type": "text/plain;charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      let result = data.items;
+      if (result.length == 0) {
+        throw new Error('Репозитория с таким именем не существует')
+      }
+      let outcome = document.getElementById("searchResult");
+      outcome.innerHTML = "";
+      let savedOutcome = document.getElementById("savedResult");
+      outcome.insertAdjacentHTML(
+        "beforeend",
+        `<button>${result[0].name}</button>`
+      );
+      outcome.insertAdjacentHTML(
+        "beforeend",
+        `<button>${result[1].name}</button>`
+      );
+      outcome.insertAdjacentHTML(
+        "beforeend",
+        `<button>${result[2].name}</button>`
+      );
+      outcome.insertAdjacentHTML(
+        "beforeend",
+        `<button>${result[3].name}</button>`
+      );
+      outcome.insertAdjacentHTML(
+        "beforeend",
+        `<button>${result[4].name}</button>`
+      );
+      outcome.onclick = function (event) {
+        let target = event.target;
+        if (target.tagName == "BUTTON") {
+          let selectedResult = result.filter(
+            (data) => data.name == target.innerHTML
+          );
+          selectedResult = selectedResult[0];
+          savedOutcome.insertAdjacentHTML(
+            "beforeend",
+            `<section><div class='saved_block'>
+              Name: ${selectedResult.name}<br>Owner: ${selectedResult.owner.login}<br>
+              Stars: ${selectedResult.stargazers_count}</div><button class='cross'></button></section>`
+          );
+          outcome.innerHTML = "";
+          formValue.value = "";
+        }
+      };
+      savedOutcome.onclick = function (event) {
+        let target = event.target;
+        if (target.tagName == "BUTTON") {
+          target.parentNode.remove();
+        }
+      };
+    })
+    .catch((err) => {
+      formValue.value = "";
+      let showError = document.getElementById("searchResult");
+      showError.innerHTML = "";
+      showError.insertAdjacentHTML(
+        "beforeend",
+        `<p>${err.message}</p>`
+      );
+    });
 }
